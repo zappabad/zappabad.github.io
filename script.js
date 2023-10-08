0c22ed444aff5eaa4c561d0e4810297e27ca7abe
Gabriel Singh Bruno <60936459+zappabad@users.noreply.github.com>
2023-10-08 19:17:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Initialize Firebase
-// Your web app's Firebase configuration (Replace with your own config)
-const firebaseConfig = {
-  apiKey: "AIzaSyAbxn0DmAIXhAVYvHSDzMWxUalgdLWjmzw",
-  authDomain: "fab-card-picker.firebaseapp.com",
-  databaseURL: "https://fab-card-picker-default-rtdb.firebaseio.com",
-  projectId: "fab-card-picker",
-  storageBucket: "fab-card-picker.appspot.com",
-  messagingSenderId: "466969619425",
-  appId: "1:466969619425:web:d83cdf766971f052f200a0",
-  measurementId: "G-0B6CRDHHEF"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-
-
-
 let currentImageElement = null;
 
 async function fetchAndDisplayCards() {
