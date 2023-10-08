1ebef037987f81d2e0faa00815673189868cab50
Gabriel Singh Bruno <60936459+zappabad@users.noreply.github.com>
2023-10-08 15:31:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-// Function to fetch and display card data
+let lastChosenImageUrl = null;
+
 async function fetchAndDisplayCards() {
     try {
-        // Fetch the card data from the JSON file
         const response = await fetch('/data/card.json');
         const cardData = await response.json();
-
-        // Get the search box element
         const searchBox = document.getElementById('searchBox');
 
-        // Listen for user input to filter cards
         searchBox.addEventListener('input', function() {
-            // Clear the card container
             const cardContainer = document.getElementById('cardContainer');
             cardContainer.innerHTML = '';
 
-            // Filter cards based on user input
             const query = searchBox.value.toLowerCase();
             const filteredCards = cardData.filter(card => card.name.toLowerCase().includes(query));
 
-            // Display the filtered cards
             filteredCards.forEach(card => {
                 const cardElement = document.createElement('div');
                 cardElement.textContent = card.name;
-                cardElement.className = 'clickable-card'; // Added a class for styling if needed
-                
-                // Add click event to show image
+                cardElement.className = 'clickable-card';
+
                 cardElement.addEventListener('click', function() {
-                    // Assume you have a designated div with id 'imageDisplay'
-                    const imageDisplay = document.getElementById('imageDisplay');
-                    // Take the first image URL from the 'printings' array
+                    const currentImageDisplay = document.getElementById('currentImage');
+                    const lastChosenImageDisplay = document.getElementById('lastChosenImage');
+                    
+                    // Update the 'last chosen' image to the last 'current' image
+                    if (lastChosenImageUrl) {
+                        lastChosenImageDisplay.innerHTML = `<img src="${lastChosenImageUrl}" alt="Last Chosen" />`;
+                    }
+                    
+                    // Update the 'current' image
                     const imageUrl = card.printings[0].image_url;
-                    // Update the imageDisplay div to show the image
-                    imageDisplay.innerHTML = `<img src="${imageUrl}" alt="${card.name}" />`;
+                    currentImageDisplay.innerHTML = `<img src="${imageUrl}" alt="${card.name}" />`;
+                    
+                    // Store the current image URL for next time
+                    lastChosenImageUrl = imageUrl;
                 });
 
                 cardContainer.appendChild(cardElement);
@@ -43,5 +44,4 @@ async function fetchAndDisplayCards() {
     }
 }
 
-// Initialize the function
 fetchAndDisplayCards();
